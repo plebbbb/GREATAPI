44986029ef1f3258b09368c41cd27325d978d031
plebbbb <genericpersonyoudontknow@gmail.com>
2021-06-10 18:58:26 -0400

    fix comment

--- include/greatapi/odometry/odometry.hpp
+++ include/greatapi/odometry/odometry.hpp
@@ -20,22 +20,22 @@ namespace greatapi{
     struct odometry {
       TWheel* Xaxis;
       TWheel* Yaxis;
-      distance X_toCOR; // distance of X axis tracking wheel to center of rotation, right is positive
+      distance X_toCOR; // distance of X axis tracking wheel to center of rotation, right is positive //TBD: figure out when measurement is positive/negative(left or right encoder)
       distance Y_toCOR; // distance of Y axis tracking wheel to center of rotation, forwards is positive
       odom_rotation* rotationcalc;
-      double encoderangoffset; //angle between the forwards direction of the bot and encoder measured positive Y axis. //TBD: figure out when measurement is positive/negative(left or right encoder)
-      double globaloffset; //angle between forwards direction and the global X axis(positive direction) at bot start position. //above tbd for this line as well
+      double encoderangoffset; //angle between the forwards direction of the bot and encoder measured positive Y axis.
+      double globaloffset; //angle between forwards direction and the local X axis(positive direction) at bot start position.
       distance Xlast = 0;
       distance Ylast = 0;
       /*
-        explaining forwardsoffset and globaloffset
+        explaining encoderangoffset and globaloffset
 
         the heading which the global coordinates use is measured as the angle between the forwards direction of the bot to the global X axis
         in order to rotate the local coordinate grid to the global axis, we need to rotate by the angle between pairs of local axises and global axises
         so that both axises are parallel with each other. This would let us add the coordinates to each other. We can apply an offset to the heading
         indicated by the global coordinates to get the angle between local and global coordinate grids.
 
-        globaloffset does this for us, as it's the offset from the forwards direction of the bot to the local X axis.
+        globaloffset does this for us, as it's the offset from the forwards direction of the bot to the local X axis, which gets the angle between global and local
 
         there is also the edge case where the local Y axis isnt alligned with the forwards direction. Globaloffset doesn't account for this
         so we can add another variable which accounts for the Y axis being offset from the forwards direction.
